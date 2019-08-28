416e17e60a7cfae82adf0472683c75532fac6df1
Joe Badaczewski <joe307bad@gmail.com>
2019-08-27 23:50:37 -0400

    adding vector map

--- wp-content/plugins/rep-finder/templates/rep-finder-template.php
+++ wp-content/plugins/rep-finder/templates/rep-finder-template.php
@@ -1,7 +1,20 @@
 <?php
 
 get_header();
+?>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.min.css" type="text/css" media="screen"/>
+  <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jvectormap@2.0.4/jquery-jvectormap.min.js"></script>
+  <script src="http://jvectormap.com/js/jquery-jvectormap-us-aea.js"></script>
+  
+  <div id="world-map" style="width: 600px; height: 400px"></div>
+  <script>
+    $(function(){
+      $('#world-map').vectorMap({map: 'us_aea'});
+    });
+  </script>
+  
 
-echo "Hello world";
+<?php
 
 get_footer();
